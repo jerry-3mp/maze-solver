82c46ed3188d2df4c0fa4d6e68081f6696d29da7
jerry-3mp <jerry.3mp@gmail.com>
2025-04-04 14:24:22 +0800

    feat: initialize project with the server folder

---
+++ maze-solver-server/src/main/java/io/jistud/mazesolver/server/MazeSolverServerApplication.java
@@ -0,0 +1,13 @@
+package io.jistud.mazesolver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeSolverServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazeSolverServerApplication.class, args);
+    }
+
+}
---
+++ maze-solver-server/src/test/java/io/jistud/mazesolver/server/MazeSolverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jistud.mazesolver.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeSolverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
