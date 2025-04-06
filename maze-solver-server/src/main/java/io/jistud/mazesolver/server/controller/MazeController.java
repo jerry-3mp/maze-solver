18565f06cdef72eb3b806c56dedff978ca982b15
jerry-3mp <jerry.3mp@gmail.com>
2025-04-06 23:12:12 +0800

    feat: [MANUAL] enable CORS in MazeController

--- maze-solver-server/src/main/java/io/jistud/mazesolver/server/controller/MazeController.java
+++ maze-solver-server/src/main/java/io/jistud/mazesolver/server/controller/MazeController.java
@@ -6,14 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.jistud.mazesolver.server.controller.dto.MazeGenerationRequestDTO;
 import io.jistud.mazesolver.server.controller.dto.MazeResponseDTO;
@@ -31,6 +24,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/v1/mazes")
+@CrossOrigin(origins = "*")
 @Tag(name = "Maze Controller", description = "API for managing mazes")
 public class MazeController {
 
